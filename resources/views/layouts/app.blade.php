1fc27ed56c51d5149b02e991e4d81ac711bb604b
Bilal Khawar <bilal.khawar@outlook.com>
2018-02-21 19:47:20 -0500

    fixed issues with font-awesome

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,7 +13,7 @@
     <link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:100,200,300,400,500,600,700,800,900" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i" rel="stylesheet">
-    <link href="{{asset('css/fontawesome-all.min.css')}}" rel="stylesheet">
+    <link href="{{asset('css/font-awesome.min.css')}}" rel="stylesheet">
     <link href="{{asset('css/devicons.min.css')}}" rel="stylesheet">
     <link href="{{asset('css/simple-line-icons.css')}}" rel="stylesheet">
     <link href="{{asset('css/resume.min.css')}}" rel="stylesheet">
@@ -307,7 +307,7 @@
                 </ul>
             </div>
         </section>
-        
+
     </div>
 
     <!-- JavaScript -->
