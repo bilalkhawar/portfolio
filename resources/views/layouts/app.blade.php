e096b5595ea11561ecb5474553f53ad2a4b05496
Bilal Khawar <bilal.khawar@outlook.com>
2018-02-25 20:53:41 -0500

    favicon support 2.0

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,11 +8,11 @@
     <meta name="description" content="A resume and a personal portfolio for Bilal Khawar">
     <meta name="author" content="Bilal Khawar">
 
-    <link rel="apple-touch-icon" sizes="180x180" href="{{asset('favicon/apple-touch-icon.png')}}">
-    <link rel="icon" type="image/png" sizes="32x32" href="{{asset('favicon/favicon-32x32.png')}}">
-    <link rel="icon" type="image/png" sizes="16x16" href="{{asset('favicon/favicon-16x16.png')}}">
-    <link rel="manifest" href="{{asset('favicon/site.webmanifest')}}">
-    <link rel="mask-icon" href="{{asset('favicon/safari-pinned-tab.svg')}}" color="#5bbad5">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{asset('apple-touch-icon.png')}}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{asset('favicon-32x32.png')}}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{asset('favicon-16x16.png')}}">
+    <link rel="manifest" href="{{asset('site.webmanifest')}}">
+    <link rel="mask-icon" href="{{asset('safari-pinned-tab.svg')}}" color="#5bbad5">
     <meta name="apple-mobile-web-app-title" content="Bilal">
     <meta name="application-name" content="Bilal">
     <meta name="msapplication-TileColor" content="#da532c">
