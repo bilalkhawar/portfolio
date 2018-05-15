fb3b7a2328f78b1c8c953e339918d9b25528930d
Bilal Khawar <bilal.khawar@outlook.com>
2018-05-15 13:43:32 -0400

    updated website

--- resources/views/projects.blade.php
+++ resources/views/projects.blade.php
@@ -3,10 +3,21 @@
         <h2 class="mb-5">Projects</h2>
         <div class="resume-item d-flex flex-column flex-md-row mb-5">
             <div class="resume-content mr-auto">
-                <h3 class="mb-0">Website Development</h3>
+                <h3 class="mb-0">BearStacks Development <a href="https://bitbucket.org/bearstacks/" target="_blank"><i class="fa fa-link"></i></a></h3>
                 <p>
-                    Made a responsive website for a real estate agent using Laravel (PHP). Developed front-end features using jQuery. Created a controls panel for the client so
-                    that the database tables could be updated.
+                    Co-Founder and Senior Developer of BearStacks Development, a company dedicated to developing innovative and practical mobile applications. Launched four
+                    applications on the Apple App Store.
+                </p>
+            </div>
+            <div class="resume-date text-md-right">
+                <span class="text-primary">2016 - Present</span>
+            </div>
+        </div>
+        <div class="resume-item d-flex flex-column flex-md-row mb-5">
+            <div class="resume-content mr-auto">
+                <h3 class="mb-0">AR Drawing</h3>
+                <p>
+                    A 3D drawing iOS application that uses SceneKit objects to construct an Augmented Reality scene.
                 </p>
             </div>
             <div class="resume-date text-md-right">
@@ -15,10 +26,44 @@
         </div>
         <div class="resume-item d-flex flex-column flex-md-row mb-5">
             <div class="resume-content mr-auto">
-                <h3 class="mb-0">BearStacks Development</h3>
+                <h3 class="mb-0">GitHub Commits</h3>
                 <p>
-                    Co-Founder and Senior Developer of BearStacks Development, a company dedicated to developing innovative and practical mobile applications. Launched four
-                    applications on the Apple App Store.
+                    An iOS app that uses Core Data to fetch and store GitHub commits.
+                </p>
+            </div>
+            <div class="resume-date text-md-right">
+                <span class="text-primary">2018</span>
+            </div>
+        </div>
+        <div class="resume-item d-flex flex-column flex-md-row mb-5">
+            <div class="resume-content mr-auto">
+                <h3 class="mb-0">Photo Sharing</h3>
+                <p>
+                    A photo sharing iOS application that uses MultipeerConnectivity.
+                </p>
+            </div>
+            <div class="resume-date text-md-right">
+                <span class="text-primary">2018</span>
+            </div>
+        </div>
+        <div class="resume-item d-flex flex-column flex-md-row mb-5">
+            <div class="resume-content mr-auto">
+                <h3 class="mb-0">Real Estate Website</h3>
+                <p>
+                    Made a responsive website for a real estate agent using Laravel (PHP). Developed front-end features using React. Designed a relational database according to
+                    the client's requirements. Created a controls panel for the client so that the database tables could be updated.
+                </p>
+            </div>
+            <div class="resume-date text-md-right">
+                <span class="text-primary">2018</span>
+            </div>
+        </div>
+        <div class="resume-item d-flex flex-column flex-md-row mb-5">
+            <div class="resume-content mr-auto">
+                <h3 class="mb-0">TypeFighter <a href="https://itunes.apple.com/us/app/typefighter/id1108705925?mt=8" target="_blank"><i class="fa fa-link"></i></a></h3>
+                <p>
+                    Developed an interactive typing game using Swift. Utilized Apple’s SpriteKit to create realistic game objects. Added Game Center integration to track high
+                    scores and global leaderboards. Designed, developed, and launched application to the App Store.
                 </p>
             </div>
             <div class="resume-date text-md-right">
@@ -27,14 +72,59 @@
         </div>
         <div class="resume-item d-flex flex-column flex-md-row mb-5">
             <div class="resume-content mr-auto">
-                <h3 class="mb-0">Scientific Calculator Pro</h3>
+                <h3 class="mb-0">Scientific Calculator Pro <a href="https://itunes.apple.com/ca/app/scientific-calculator-pro/id1109819405?mt=8" target="_blank"><i class="fa fa-link"></i></a></h3>
                 <p>
-                    Developed a scientific calculator using Swift. Implemented UIStackView for the layout and designed the app logo in Gimp. The application is currently
-                    available on the Apple App Store.
+                    Developed a scientific calculator using Swift. Implemented a stack-based algorithm for parsing mathematical expressions. Used Size Classes and designed the
+                    app logo in Gimp. The application is currently available on the Apple App Store.
                 </p>
             </div>
             <div class="resume-date text-md-right">
                 <span class="text-primary">2016</span>
+            </div>
+        </div>
+        <div class="resume-item d-flex flex-column flex-md-row mb-5">
+            <div class="resume-content mr-auto">
+                <h3 class="mb-0">Find My Country <a href="https://itunes.apple.com/us/app/find-my-country/id1109819780?mt=8" target="_blank"><i class="fa fa-link"></i></a></h3>
+                <p>
+                    A geography game programmed in Objective-C. Used a third party library called WhirlyGlobe-Maply. This application is currently available on the Apple App Store.
+                </p>
+            </div>
+            <div class="resume-date text-md-right">
+                <span class="text-primary">2016</span>
+            </div>
+        </div>
+        <div class="resume-item d-flex flex-column flex-md-row mb-5">
+            <div class="resume-content mr-auto">
+                <h3 class="mb-0">Numtate <a href="https://itunes.apple.com/us/app/numtate/id1109819497?mt=8" target="_blank"><i class="fa fa-link"></i></a></h3>
+                <p>
+                    A puzzle game that uses UIKit Animations. This application is currently available on the Apple App Store.
+                </p>
+            </div>
+            <div class="resume-date text-md-right">
+                <span class="text-primary">2016</span>
+            </div>
+        </div>
+        <div class="resume-item d-flex flex-column flex-md-row mb-5">
+            <div class="resume-content mr-auto">
+                <h3 class="mb-0">Power Supply Converter</h3>
+                <p>
+                    Designed a power supply converter that converts 12V AC into +5V DC and +12V DC by building a rectifier and soldering various components on the PCB.
+                </p>
+            </div>
+            <div class="resume-date text-md-right">
+                <span class="text-primary">2015</span>
+            </div>
+        </div>
+        <div class="resume-item d-flex flex-column flex-md-row mb-5">
+            <div class="resume-content mr-auto">
+                <h3 class="mb-0">Memory Swipe</h3>
+                <p>
+                    Launched this iOS game application on the Apple App Store. The game presents the user with a randomly drawn pattern of lines, which the user has to redraw
+                    once the lines disappear. Demonstrated an MVC framework while implementing the singleton and delegate design patterns in Objective-C.
+                </p>
+            </div>
+            <div class="resume-date text-md-right">
+                <span class="text-primary">2015</span>
             </div>
         </div>
         <div class="resume-item d-flex flex-column flex-md-row mb-5">
@@ -46,6 +136,17 @@
             </div>
             <div class="resume-date text-md-right">
                 <span class="text-primary">2015</span>
+            </div>
+        </div>
+        <div class="resume-item d-flex flex-column flex-md-row mb-5">
+            <div class="resume-content mr-auto">
+                <h3 class="mb-0">Automated Window System</h3>
+                <p>
+                    Demonstrated good communication and teamwork skills while working on a team to design Automated Window System that reduces energy consumption.
+                </p>
+            </div>
+            <div class="resume-date text-md-right">
+                <span class="text-primary">2012</span>
             </div>
         </div>
     </div>
